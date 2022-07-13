7bd30617074689781bfc2ca97f8901aa1e6d549f
Soojin Hwang <soojiny.hwang@gmail.com>
2022-07-13 18:23:30 +0100

    some examples of writing JSX syntax

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -10,12 +10,38 @@ const root = ReactDOM.createRoot(el);
 
 // 4) Create a Component
 function App() {
+  return (
+    <div className="wrapper">
+      <textarea
+        readOnly
+        maxLength={3}
+        spellCheck
+        style={{backgroundColor: 'gray'}}
+      />
+    </div>
+  );
+
   // let message = "Bye there!"
   // if (Math.random() > 0.5) {
   //   message = "Hello there!";
   // }
+  // return <h1>{new Date().toLocaleTimeString()}</h1>;
 
-  return <h1>{new Date().toLocaleTimeString()}</h1>;
+  // const name = "Samantha";
+  // const age = 23;
+  // return (
+  //   <h1>
+  //     Hi, my name is {name} and my age is {age}
+  //   </h1>
+  // );
+
+  // const inputType = "number";
+  // const minValue = 5;
+  // return <input type={inputType} min={minValue} />;
+  // return <input style={{ border: "3px solid red" }} type="number" min={5} />;
+
+
+
 }
 
 // 5) Show the component on the screen
