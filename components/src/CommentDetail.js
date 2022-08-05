191da94aa724d8e5906e3eb261add71b635ce154
Soojin Hwang <soojiny.hwang@gmail.com>
2022-08-05 17:02:34 +0100

    refactored with props

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { faker } from "@faker-js/faker";
+// import { faker } from '@faker-js/faker';
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()}/>
+        <img alt="avatar" src={props.avatar}/>
       </a>
       <div className="content">
       <a href="/" className="author">
-        Sam
+        {props.author}
       </a>
       <div className="metadata">
         <span className="date">
-          Today at 6:00 PM
+          {props.timeAgo}
         </span>
       </div>
       <div className="text">
-        Nice blog post!
+        {props.body}
       </div>
       </div>
     </div>
--- components/src/index.js
+++ components/src/index.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { faker } from "@faker-js/faker";
+// React 17 method
+// import ReactDOM from 'react-dom';
 
+// React 18 method
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { faker } from '@faker-js/faker';
 import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        body="Nice blog post!"
+      />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        body="That's good to know"
+      />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        body="Cool"
+      />
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+// React 18 method
+const root = createRoot(document.querySelector('#root'));
+root.render(<App />);
+
+// React 17 method
+// ReactDOM.render(<App />, document.querySelector('#root'))
